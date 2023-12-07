8fd4519f01cd4d571993f2f49d651f2bfd77775a
Abdi <aabdymohamed@gmail.com>
2023-12-07 16:02:16 +0100

    first shit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace progam {
+    class Program{
+
+        class Person {
+            public string Name {get; set;}
+            public int Age {get; set;}
+        }
+ 
+ 
+static void Main(){
+  List<Person> people = new List<Person>
+        {
+            new Person { Name = "Alice", Age = 25 },
+            new Person { Name = "Bob", Age = 30 },
+            new Person { Name = "Charlie", Age = 22 }
+        };
+        int [] luckyNumbers = {0,56,32,10,1,4,6};
+        luckyNumbers[1] = 36;
+        Console.WriteLine(luckyNumbers[1]);
+}
+};
+
+ }  
